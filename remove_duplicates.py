4a8d37e1d15143215725d626218e5476d2069289
TheGoldKay <jhonnatanwfc@live.com>
2022-11-10 08:14:13 -0300

    remove dubs

---
+++ remove_duplicates.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        s = list(s)
+        i = self.duplicates(s)
+        while i:
+            del s[i:i+2]
+            i = self.duplicates(s)
+        return "".join(s)
+
+    def duplicates(self, s):
+        for i in range(len(s)-1):
+            if s[i] == s[i+1]:
+                #print(s[i], s[i+1])
+                return i
+        return None 
